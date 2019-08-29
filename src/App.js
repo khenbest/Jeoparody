94b1ddb88bc92cde179c0737e6a04bfd5d4a0fbb
Kenny Henbest <lekenbest@gmail.com>
2019-08-29 14:29:49 -0600

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={"https://www.jeopardy.com/sites/default/files/styles/practice_test/public/2018-07/Jeopardy_2k_HeroLogo-4x3.jpg?itok=sn07ulDF"} className="App-logo" alt="logo" />
+        <p>Yo dawg</p>
       </header>
     </div>
   );
